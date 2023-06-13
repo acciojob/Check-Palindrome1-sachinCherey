7c14f5752910c9e1c2ad6f758524f1b450e562a0
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-06-14 01:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+   let a=str.split(" ").join("");
+   let b=str.split(" ").reverse().join("");	
+   a.toLowerCase();
+   b.toLowerCase();
+   if(a==b)return true;
+    return false;	
+	
 }
 module.exports = palindrome
